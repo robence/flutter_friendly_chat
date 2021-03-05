95c34522b922b293b0dcb8a934a84ba36b7fe634
Bence Rozsa <robence@gmail.com>
2021-03-05 06:50:52 +0100

    feat(ChatScreen): improve alignment, add send button

--- lib/main.dart
+++ lib/main.dart
@@ -28,16 +28,30 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final _textController = TextEditingController();
   void _handleSubmitted(String text) {
+    print(text);
     _textController.clear();
   }
 
   Widget _buildTextComposer() {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 8.0),
-      child: TextField(
-          controller: _textController,
-          onSubmitted: _handleSubmitted,
-          decoration: InputDecoration.collapsed(hintText: "Send a message")),
+      margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+      child: Row(
+        children: [
+          Flexible(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration: InputDecoration.collapsed(hintText: "Send a message"),
+            ),
+          ),
+          Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: const Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text),
+              ))
+        ],
+      ),
     );
   }
 

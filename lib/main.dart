98a62c44fe84b3c0003d0430b867c4ff2d7e137a
Bence Rozsa <robence@gmail.com>
2021-03-07 06:30:01 +0100

    feat(ChatScreen): show chat messages

--- lib/main.dart
+++ lib/main.dart
@@ -58,10 +58,17 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = [];
   final _textController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    ChatMessage message = ChatMessage(text: text);
+    setState(() {
+      _messages.insert(0, message);
+    });
+    _focusNode.requestFocus();
   }
 
   Widget _buildTextComposer() {
@@ -74,6 +81,7 @@ class _ChatScreenState extends State<ChatScreen> {
               controller: _textController,
               onSubmitted: _handleSubmitted,
               decoration: InputDecoration.collapsed(hintText: "Send a message"),
+              focusNode: _focusNode,
             ),
           ),
           IconTheme(
@@ -95,7 +103,26 @@ class _ChatScreenState extends State<ChatScreen> {
       appBar: AppBar(
         title: Text("FriendlyChat"),
       ),
-      body: _buildTextComposer(),
+      body: Column(
+        children: [
+          Flexible(
+              child: ListView.builder(
+            padding: EdgeInsets.all(8.0),
+            reverse: true,
+            itemBuilder: (_, int index) => _messages[index],
+            itemCount: _messages.length,
+          )),
+          Divider(
+            height: 1.0,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardColor,
+            ),
+            child: _buildTextComposer(),
+          )
+        ],
+      ),
     );
   }
 }
